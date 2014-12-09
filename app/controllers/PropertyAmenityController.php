5dd3cba8a1882534fa819408f0df5a1cbc112759
Janssen Canturias <janssen.canturias@gmail.com>
2014-12-10 06:21:42 +0800

    - Property Amenity Resource

--- app/controllers/PropertyAmenityController.php
+++ app/controllers/PropertyAmenityController.php
@@ -24,5 +24,35 @@ class PropertyAmenityController extends BaseController{
             return $this->makeFailResponse("Property (ID = $propertyId) does not exists.");
         }
     }
+    /**
+     * Stores an amenity resource for a property resource
+     * 
+     * @param id $propertyId
+     * @return Response
+     */
+    public function store($propertyId) {
+        if($this->entityExists('property', $propertyId)) {
+            $rules = array(
+                "name" => "required|max:64", 
+                "description" => "required"
+            );
+            $validation = Validator::make(Input::all(), $rules);
+            if($validation->fails()) {
+                return $this->makeFailResponse("Amenity creation failed due to validation errors.", $validation->messages()->getMessages());
+            }
+            else {
+                $amenity = new Amenity();
+                $amenity->name = Input::get("name");
+                $amenity->description = Input::get("description");
+                $amenity->property_id = $propertyId;
+                $amenity->save();
+                
+                return $this->makeSuccessResponse("Amenity resource created", $amenity->toArray());
+            }
+        }
+        else {
+            return $this->makeFailResponse("Property (ID = $propertyId) does not exists.");
+        }
+    }
     
 }
