034acd999c9319ef57388dc75beb39929d62dd30
Janssen Canturias <janssen.canturias@gmail.com>
2014-12-25 04:16:46 +0800

    Cloudinary Integration

--- app/libraries/Helpers/FileManager/PhotoManager.php
+++ app/libraries/Helpers/FileManager/PhotoManager.php
@@ -8,8 +8,6 @@
 
 namespace Helpers\FileManager;
 
-//use Symfony\Component\HttpFoundation\File\UploadedFile;
-
 class PhotoManager {
 
     private $api;
@@ -18,6 +16,15 @@ class PhotoManager {
         $this->api = new \Cloudinary\Api();
     }
 
+    /**
+     * Creates a photo resource from a cloudinary image
+     * 
+     * @param Cloudinary Public ID $public_id
+     * @param Eloquent $model
+     * @param String $caption
+     * @param array $trnsfrsmtn
+     * @return Photo
+     */
     public function createCloudinary($public_id, \Eloquent $model, $caption = null, array $trnsfrsmtn = null) {
         $time = time();
         $userId = \Auth::id();
@@ -25,10 +32,10 @@ class PhotoManager {
         $resource = $this->api->resource($public_id);
         $fileName = "$model->id-$userId-$time";
         if ($model instanceof \User) {
-            $path = "users/$fileName";
+            $path = "users/$model->id/$fileName";
             $tags = "user";
         } else if ($model instanceof \Property) {
-            $path = "properties/$fileName";
+            $path = "properties/$model->id/$fileName";
             $tags = "property";
         } else if ($model instanceof \Amenity) {
             $path = "properties/$model->property_id/amenities/$fileName";
@@ -89,60 +96,5 @@ class PhotoManager {
         $photo->save();
 
         return $photo;
-    }
-
-    public function create(UploadedFile $uploadedFile, $type, $typeId, $caption = null) {
-        $userId = is_null(\Auth::id()) ? 0 : \Auth::id();
-
-        $time = time();
-        $extension = $uploadedFile->getClientOriginalExtension();
-
-        if ($type == 'property') {
-            $property = \Property::find($typeId);
-            $fileName = "$property->id-$userId-$time.$extension";
-            $path = "/uploads/properties/$property->id";
-        } else if ($type == 'user') {
-            $user = \User::find($typeId);
-            $fileName = "$user->id-$userId-$time.$extension";
-            $path = "/uploads/users";
-        } else if ($type == 'amenity') {
-            $amenity = \Amenity::find($typeId);
-            $fileName = "$amenity->id-$userId-$time.$extension";
-            $path = "/uploads/properties/$amenity->property_id/amenities";
-        } else if ($type == 'content') {
-            $content = \Content::find($typeId);
-            $fileName = "$content->id-$userId-$time.$extension";
-            $path = "/uploads/contents";
-        } else if ($type == 'developer') {
-            $developer = \Developer::find($typeId);
-            $fileName = "$developer->id-$userId-$time.$extension";
-            $path = "/uploads/developers";
-        } else if ($type == 'unit') {
-            $unit = \Unit::find($typeId);
-            $fileName = "$unit->id-$userId-$time.$extension";
-            $path = "/uploads/properties/$unit->property_id/units/$unit->id";
-        } else {
-            return null;
-        }
-
-        $publicPath = public_path() . "$path";
-        if (!\File::isDirectory($publicPath)) {
-            \File::makeDirectory($publicPath, 0775, true);
-        }
-
-        $moved = $uploadedFile->move($publicPath, $fileName);
-
-        if ($moved) {
-            $photo = new \Photo();
-            $photo->path = "$publicPath/$fileName";
-            $photo->url = \URL::to("$path/$fileName");
-            $photo->uploaded_by = $userId;
-            $photo->caption = $caption;
-            $photo->save();
-
-            return $photo;
-        } else {
-            return null;
-        }
     }
 }
