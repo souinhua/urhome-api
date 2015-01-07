d50d7fb9ab3e60ae0e5c8be36c2477355c9f9fce
Janssen Canturias <janssen.canturias@gmail.com>
2015-01-08 04:21:36 +0800

    Update API Response

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -93,7 +93,7 @@ class UserController extends \BaseController {
             $rules = array(
                 "name" => "max:64",
                 "title" => "max:32",
-                "email" => "unique:user,email",
+                "email" => "max:64",
                 "phone" => "max:32",
                 "acl_id" => "exists:acl,id",
                 "password" => "min:6",
