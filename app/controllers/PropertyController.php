093d1dc4893c4b4c6d22f39151877f0a5eab55e1
Janssen Canturias <janssen.canturias@gmail.com>
2015-02-06 06:50:23 +0800

    Property Filter

--- app/controllers/PropertyController.php
+++ app/controllers/PropertyController.php
@@ -58,15 +58,11 @@ class PropertyController extends \BaseController {
             $query = $query->join("common_details", "property.common_details_id", "=", "common_details.id")->select('property.*');
 
             if (Input::has('min_price')) {
-                $query = $query->where(function($query) {
-                    $query
-                            ->where("common_details.min_price", "<=", Input::get("min_price"))
-                            ->where("common_details.max_price", ">=", Input::get("min_price"));
-                });
+                $query = $query->where("common_details.min_price", ">=", Input::get("min_price"));
             }
 
             if (Input::has('max_price')) {
-                $query = $query->where("common_details.max_price", ">=", Input::get("max_price"));
+                $query = $query->where("common_details.max_price", "<=", Input::get("max_price"));
             }
 
             if (Input::has('bed')) {
