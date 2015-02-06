7d3106fdfe6dfcc8b42af63264143871b4af1c49
Janssen Canturias <janssen.canturias@gmail.com>
2015-02-07 07:32:33 +0800

    Property Filter

--- app/controllers/AddressController.php
+++ app/controllers/AddressController.php
@@ -1,21 +1,15 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of AddressController
  *
  * @author User
  */
 class AddressController extends BaseController {
-    
+
     public function properties() {
-        $table = DB::select("select distinct a.city, a.province from property p inner join address a on a.id = p.address_id");
+        $table = DB::select("select distinct a.city, a.province, lower(replace(concat(a.city,' ',a.province),' ','-')) slug from address a inner join property p on p.address_id = a.id");
         return $this->makeResponse($table, 200, "Distinct Property Locations fetched.");
     }
-    
+
 }
